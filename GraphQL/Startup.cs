91e1c24fc9ee9a779371f7feccb97fe83f62862c
Mohamed Nady <mohamed.nady@hindawi.local>
2018-04-29 18:15:09 +0200

    adding

---
+++ GraphQL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GraphQL.Startup))]
+namespace GraphQL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
